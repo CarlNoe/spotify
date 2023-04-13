0a50df58f73fe45b8032ca0de9a68b2762e44097
Noe Carl <noe.carldev@gmail.com>
2023-04-13 23:41:32 +0200

    Initialized Playlists page and slice

---
+++ src/features/Playlists/Playlists.tsx
@@ -0,0 +1,12 @@
+import "./Playlists.scss";
+
+function Playlists() {
+  return (
+    <div>
+      <div>Playlists</div>
+      <div>Playlists</div>
+    </div>
+  );
+}
+
+export default Playlists;
